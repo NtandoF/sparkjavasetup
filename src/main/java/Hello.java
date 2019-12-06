d95cef421e6cf521475aebb34f2afacf21b0e0fc
Your Name <you@example.com>
2019-12-06 12:54:24 +0200

    added a space

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,9 +6,25 @@ import java.util.Map;
 
 public class Hello {
 
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
+    public static void main(String[] args) {
+        staticFiles.location("/public"); // Static files
+
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> {
+            return new ModelAndView(new HashMap<>(), "hello.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/hello", (req, res) -> {
             Map<String, String> dataMap = new HashMap<>();
             dataMap.put("name", "Ntando");
             return new ModelAndView(dataMap, "hello.hbs");
